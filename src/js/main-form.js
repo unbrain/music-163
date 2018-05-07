400eb691c7e2b13de9234233d8c6293fd2aba0ae
unbrain <marsorsun@gmail.com>
2018-05-07 23:30:38 +0800

    添加外链到main

--- src/js/main-form.js
+++ src/js/main-form.js
@@ -6,7 +6,7 @@
                 <div class="songEditTitle">编辑歌曲信息</div>
                 <div class="row">
                     <label for="">歌名:
-                        <input type="text">
+                        <input type="text" value="__key__">
                     </label>
                 </div>
                 <div class="row">
@@ -16,15 +16,20 @@
                 </div>
                 <div class="row">
                     <label for="">外链:
-                        <input type="text">
+                        <input type="text" value="__link__">
                     </label>
                 </div>
                 <div class="row">
                     <button type="submit">保存</button>
                 </div>
             </form>`,
-        render(data){
-            $(this.el).html(this.template)
+        render(data = {}){
+            let palceholders = ['key', 'link']
+            let html = this.template
+            palceholders.map((string) => {
+                html = html.replace(`__${string}__`, data[string] || '')
+            })
+            $(this.el).html(html)
         }
 
     }
@@ -35,8 +40,7 @@
             this.moudle = moudle
             this.view.render(this.moudle.data)
             window.eventHub.on('upload', (data) => {
-                console.log('main form')
-                console.log(data)
+                this.view.render(data)
             })
         }
     }
